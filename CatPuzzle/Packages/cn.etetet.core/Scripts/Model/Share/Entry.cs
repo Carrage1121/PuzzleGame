32212ce4975f4a3ea745562f28130897e2886e0c
behappy <1208712478@qq.com>
2024-12-09 20:58:37 +0800

    delete ignore

---
+++ CatPuzzle/Packages/cn.etetet.core/Scripts/Model/Share/Entry.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace ET
+{
+    public struct EntryEvent1
+    {
+    }   
+    
+    public struct EntryEvent2
+    {
+    } 
+    
+    public struct EntryEvent3
+    {
+    }
+    
+    public static class Entry
+    {
+        public static void Init()
+        {
+            
+        }
+        
+        public static void Start()
+        {
+            StartAsync().NoContext();
+        }
+        
+        private static async ETTask StartAsync()
+        {
+            //todo
+            //根据操作系统调整时间精度？
+            WinPeriod.Init();
+
+            // 注册Mongo type
+            MongoRegister.Init();
+            
+            //MemoryPackage注册
+            MemoryPackRegister.Init();
+            
+            // 注册Entity序列化器
+            EntitySerializeRegister.Init();
+
+            World.Instance.AddSingleton<SceneTypeSingleton, Type>(typeof(SceneType));
+            //对象池
+            World.Instance.AddSingleton<ObjectPool>();
+            //id生成 所有的id唯一
+            World.Instance.AddSingleton<IdGenerater>();
+            
+            //todo
+            //网络相关？
+            World.Instance.AddSingleton<OpcodeType>();
+            
+            //todo
+            //消息队列？
+            World.Instance.AddSingleton<MessageQueue>();
+            
+            //todo
+            //网络相关？
+            World.Instance.AddSingleton<NetServices>();
+            
+            //todo
+            LogMsg logMsg = World.Instance.AddSingleton<LogMsg>();
+            logMsg.AddIgnore(LoginOuter.C2G_Ping);
+            logMsg.AddIgnore(LoginOuter.G2C_Ping);
+            
+            // 创建需要reload的code singleton
+            CodeTypes.Instance.CodeProcess();
+            
+            await World.Instance.AddSingleton<ConfigLoader>().LoadAsync();
+            
+            await FiberManager.Instance.Create(SchedulerType.Main, SceneType.Main, 0, SceneType.Main, "");
+        }
+    }
+}
---
+++ CatPuzzle/Packages/cn.etetet.loader/Scripts/Loader/Client/Init.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using CommandLine;
+using UnityEngine;
+
+namespace ET
+{
+    public class Init: MonoBehaviour
+    {
+        private void Start()
+        {
+            this.StartAsync().NoContext();
+        }
+		
+        private async ETTask StartAsync()
+        {
+            DontDestroyOnLoad(gameObject);
+			
+            AppDomain.CurrentDomain.UnhandledException += (sender, e) =>
+            {
+                Log.Error(e.ExceptionObject.ToString());
+            };
+
+            // 命令行参数
+            string[] args = "".Split(" ");
+            Parser.Default.ParseArguments<Options>(args)
+                    .WithNotParsed(error => throw new Exception($"命令行格式错误! {error}"))
+                    .WithParsed((o)=>World.Instance.AddSingleton(o));
+
+            //获取globalConfig
+            GlobalConfig globalConfig = Resources.Load<GlobalConfig>("GlobalConfig");
+            //绑定场景名
+            Options.Instance.SceneName = globalConfig.SceneName;
+			
+            //输出
+            World.Instance.AddSingleton<Logger>().Log = new UnityLogger();
+            ETTask.ExceptionHandler += Log.Error;
+			
+            //时间
+            World.Instance.AddSingleton<TimeInfo>();
+            
+            //todo
+            //纤程？
+            World.Instance.AddSingleton<FiberManager>();
+
+            //todo
+            //创建默认包
+            await World.Instance.AddSingleton<ResourcesComponent>().CreatePackageAsync("DefaultPackage", true);
+            
+            //CodeLoad开启游戏
+            World.Instance.AddSingleton<CodeLoader>().Start().NoContext();
+        }
+
+        private void Update()
+        {
+            TimeInfo.Instance.Update();
+            FiberManager.Instance.Update();
+        }
+
+        private void LateUpdate()
+        {
+            FiberManager.Instance.LateUpdate();
+        }
+
+        private void OnApplicationQuit()
+        {
+            World.Instance.Dispose();
+        }
+    }
+}
